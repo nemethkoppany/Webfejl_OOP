a8e0e72ca9fd76b6b7b9ed6ddce8e59e95f47c61
Nemeth Koppany Zoltan <nemeth.koppany@BOLYAI.LOCAL>
2024-11-29 15:30:57 +0100

    player after 2015

--- basics/scripts.js
+++ basics/scripts.js
@@ -1,26 +1,50 @@
-function player(nickname){
-    this.nickname = nickname;
-    this.playedMatch = 0;
-}
-player.prototype.play = 
-function(){
-this.playedMatch++;
-console.log(this.nickname, this.playedMatch);
+// function player(nickname){
+//     this.nickname = nickname;
+//     this.playedMatch = 0;
+// }
+// player.prototype.play = 
+// function(){
+// this.playedMatch++;
+// console.log(this.nickname, this.playedMatch);
+// }
+
+// player.prototype.getTierLevel =
+// function(){
+//     if(this.playedMatch < 4){
+//         return "A";
+//     }
+//     else if (this.playedMatch > 3 && this.playedMatch < 7) {
+//         return "B";
+//     } else {
+//         return "C";
+//     }
+// }
+
+class player{
+    constructor(nickname){
+        this.nickname = nickname;
+        this.playedMatch = 0;
+    }
+
+    play(){
+         this.playedMatch++;
+        console.log(this.nickname, this.playedMatch);
+    }
+
+    getTierLevel(){
+        if(this.playedMatch < 4){
+                    return "A";
+                }
+                else if (this.playedMatch > 3 && this.playedMatch < 7) {
+                    return "B";
+                } else {
+                    return "C";
+                }
+    }
 }
 
-player.prototype.getTierLevel =
-function(){
-    if(this.playedMatch < 4){
-        return "A";
-    }
-    else if (this.playedMatch > 3 && this.playedMatch < 7) {
-        return "B";
-    } else {
-        return "C";
-    }
-}
 
-const gomszab = new player("gomszab")
+const gomszab = new player("gomszab");
 
 gomszab.play();
 gomszab.getTierLevel();
