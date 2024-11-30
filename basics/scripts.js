f4cf4003e6838ce29dbb0f2a28117087abad7d37
nemethkoppany <koppany344@gmail.com>
2024-11-30 20:25:21 +0100

    A 16. feladat nem megy

--- basics/scripts.js
+++ basics/scripts.js
@@ -21,45 +21,69 @@
 //     }
 // }
 
-function printTierLevel(player){
+// function printTierLevel(player){
   
 
-   console.log(player.nickname, player.getTierLevel());
+//    console.log(player.nickname, player.getTierLevel());
 
+// }
+
+
+//_______________________________________________________________________________________
+//12.feladattól
+function Person(){
+    this.name = "Géza";
 }
 
-
-class player{
-    constructor(nickname){
-        this.nickname = nickname;
-        this.playedMatch = 0;
-    }
-
-    play(){
-         this.playedMatch++;
-        console.log(this.nickname, this.playedMatch);
-    }
-
-    getTierLevel(){
-        if(this.playedMatch < 4){
-                    return "A";
-                }
-                else if (this.playedMatch > 3 && this.playedMatch < 7) {
-                    return "B";
-                } else {
-                    return "C";
-                }
-    }
-    printTierLevel(){
-        console.log(this.nickname, this.getTierLevel());
-    }
+Person.prototype.GetName =
+function(){
+    return this.name;
 }
 
+Person.prototype.Student= 
+function(school){
+    this.school = school;
+}
+//Object.setPrototypeOf(Student.prototype, Person.prototype)
 
-const gomszab = new player("gomszab");
 
-gomszab.play();
-gomszab.getTierLevel();
-console.log(gomszab.getTierLevel());
-// printTierLevel(gomszab);
-gomszab.printTierLevel();
+
+//_______________________________________________________________________________________
+
+// class player{
+//     constructor(nickname){
+//         this.nickname = nickname;
+//         this.playedMatch = 0;
+//     }
+
+//     play(){
+//          this.playedMatch++;
+//         console.log(this.nickname, this.playedMatch);
+//     }
+
+//     getTierLevel(){
+//         if(this.playedMatch < 4){
+//                     return "A";
+//                 }
+//                 else if (this.playedMatch > 3 && this.playedMatch < 7) {
+//                     return "B";
+//                 } else {
+//                     return "C";
+//                 }
+//     }
+//     printTierLevel(){
+//         console.log(this.nickname, this.getTierLevel());
+//     }
+// }
+
+
+//const gomszab = new player("gomszab");
+const person = new Person();
+console.log(person.GetName());
+
+
+// gomszab.play();
+// gomszab.getTierLevel();
+// console.log(gomszab.getTierLevel());
+ // printTierLevel(gomszab);
+// gomszab.printTierLevel();
