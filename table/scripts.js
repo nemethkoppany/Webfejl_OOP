8218ceae8ee3e9ebdd915650cb1ad57117496f54
nemethkoppany <nemeth.koppany06@gmail.com>
2024-12-13 15:01:23 +0100

    táblázat legenerálása

--- table/scripts.js
+++ table/scripts.js
@@ -19,3 +19,44 @@ const array = [
         lastname: 'Horváth'
     },
 ]
+
+class Person{
+    constructor(obj){
+        this.firstname1 = obj.firstname1;
+        this.firstname2 = obj.firstname2;
+        this.lastname = obj.lastname;
+    }
+    render(parentElement){
+        const row = document.createElement("tr");
+        parentElement.appendChild(row);
+
+        const td_cell3 = document.createElement("td");
+        td_cell3.innerHTML = this.lastname;
+        row.appendChild(td_cell3);
+
+        const td_cell = document.createElement("td");
+        td_cell.innerHTML = this.firstname1;
+        row.appendChild(td_cell);
+
+        if(this.firstname2 !== undefined){
+            const td_cell2 = document.createElement("td");
+            td_cell2.innerHTML = this.firstname2;
+            row.appendChild(td_cell2);
+        }
+        else{
+            td_cell.colSpan = "2";
+        }
+        
+        
+
+    }
+}
+
+function init(){
+    for(const pers of array){
+        const person = new Person(pers);
+        person.render(document.getElementById("tbodyId"));
+    }
+}
+
+init();
