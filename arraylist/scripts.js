49a4aef9a040069a6fc1226174239870040db26c
Nemeth Koppany Zoltan <nemeth.koppany@BOLYAI.LOCAL>
2025-01-17 15:31:21 +0100

    órai anyag

---
+++ arraylist/scripts.js
@@ -0,0 +1,71 @@
+class ArrayList{
+    /**
+     * Ez tárolja el az ArrayList hosszát
+     * @type{Number}
+     */
+    #lenght
+    #state
+
+    get Count(){
+        return this.#lenght;
+    }
+
+    constructor(){
+        this.#lenght = 0;
+        this.#state = {};
+    }
+    Add(item){
+        //Hossz megnövelés
+        //Belső objetum beletétele
+        //Példány aktuális hosszának eltárolása egy változóban
+
+        //1.
+        const index = this.#lenght;
+
+        //2.
+        //A belső objektum index tulajdonságának megadjuk a bemeneti paraméter értékét
+        this.#state[index] = item;
+
+        //Az aktuális indexel el akarjuk érni(pl.: array[0]) a hozzáadott elemet a példányon keresztül
+        Object.defineProperty(this, index, {
+            get: function(){
+                return this.#state[index]
+            },
+            set:function(value){
+                this.#state[index] = value
+            },
+            writable: true
+        })
+
+        //3.
+        //Inkrementáljuk a lenght tulajdonságot
+        this.#lenght++;
+    }
+    Clear(){
+        this.#lenght = 0;
+        this.#state = {};
+    }
+   
+}
+/*
+const pers = {};
+pers.a = "Ferenc";
+pers["a"] = "Józsi";
+pers[0] = "tojás";
+console.log(pers);
+*/
+//_________________________________________________________________________
+ //Object.defineProperty()
+    //1. objektum érték
+    //2. tulajdonság név(string)
+    //3. descriptor objektum
+
+/*
+const kakas = {};
+Object.defineProperty(kakas, "név", {
+    value: "Géza",
+    writable: true
+})
+kakas.név = "asd";
+console.log(kakas)
+*/
